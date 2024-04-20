cc2ac877442c192892a53519c1f92abf7535ec14
root <root@debug.titan>
2024-04-20 14:14:15 +0000

    TokenMintScanner

--- src/config.js
+++ src/config.js
@@ -1,13 +1,16 @@
 import {PublicKey, clusterApiUrl} from "@solana/web3.js";
 import 'dotenv/config.js';
-const  HTTP_ENDPOINT= process.env.HTTP_ENDPOINT
-const WSS_ENDPOINT= process.env.WSS_ENDPOINT || "wss://api.mainnet-beta.solana.com" 
+
 
 export const  Config = {
     PROGRAM_ID: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
-    CONNECTION: HTTP_ENDPOINT,
+    CONNECTION1: process.env.HTTP_ENDPOINT1,
+    CONNECTION2: process.env.HTTP_ENDPOINT2,
+    CONNECTION3: process.env.HTTP_ENDPOINT3,
+    CONNECTION4: process.env.HTTP_ENDPOINT4,
+    CONNECTION5: process.env.HTTP_ENDPOINT5,
     RPC_CONNECTION: clusterApiUrl('mainnet-beta'),
-    WEBSOCKET_CONNECTION: WSS_ENDPOINT,
+    WEBSOCKET_CONNECTION: "wss://api.mainnet-beta.solana.com" ,
     BOT_API_TOKEN: process.env.BOT_API_TOKEN,
     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID
 }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 import { Connection, PublicKey } from "@solana/web3.js";
 import { Metaplex } from "@metaplex-foundation/js";
-import WebSocket from "ws";
 import { Telegraf, Markup } from "telegraf";
 import { formatNumber } from "./utils.js";
 import { Config } from "./config.js";
 
-const sleep = ms => {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
+
 const bot = new Telegraf(Config.BOT_API_TOKEN);
 const solanaConnection = new Connection(Config.RPC_CONNECTION, { wsEndpoint: Config.WEBSOCKET_CONNECTION });
-const web3 = new Connection(Config.CONNECTION);
-const metaplex = Metaplex.make(web3);
+
 
 const startEvent = () => {
   solanaConnection.onLogs(
     Config.PROGRAM_ID,
     ({ logs, err, signature }) => {
       if (logs && logs.some(log => log.includes("initializeMint"))) {
-        getTokenMint(signature)
+        let scanner = new TokenMintScanner(signature)
+        scanner.getTokenMint()
 
       } else if (logs && logs.some(log => log.includes("InitializeMint"))) {
-        getTokenMint(signature)
+        let scanner = new TokenMintScanner(signature)
+        scanner.getTokenMint()
       }
       else if (logs && logs.some(log => log.includes("InitializeMint2"))) {
-        getTokenMint(signature)
+        let scanner = new TokenMintScanner(signature)
+        scanner.getTokenMint()
 
       }
       else if (logs && logs.some(log => log.includes("initializeMint2"))) {
-        getTokenMint(signature)
+        let scanner = new TokenMintScanner(signature)
+        scanner.getTokenMint()
 
       }
     },
@@ -37,111 +37,163 @@ const startEvent = () => {
 };
 
 
-async function getTokenMint(signature) {
-  try {
-    await sleep(30000)
-    const transaction = await web3.getParsedTransaction(
-      signature,
-      {
-        maxSupportedTransactionVersion: 0,
-      }
-    );
-    //console.log(transaction,signature)
-    if (transaction && transaction.transaction) {
-      transaction.transaction.message.instructions.forEach(instruction => {
-        // Further logic to verify if this instruction is a token creation
-        if(instruction.parsed && instruction.parsed.type){
-          if (instruction.parsed.type == "initializeMint") {
-            console.log(instruction.parsed.info)
-            getTokenMeta(
-              instruction.parsed.info.mint,signature
-            );
-          } else if (instruction.parsed.type == "InitializeMint") {
-            getTokenMeta(
-              instruction.parsed.info.mint,signature
-            );
-          } else if (instruction.parsed.type == "initializeMint2") {
-            getTokenMeta(
-              instruction.parsed.info.mint,signature
-            );
-          }
-          else if (instruction.parsed.type == "InitializeMint2") {
-            getTokenMeta(
-              instruction.parsed.info.mint,signature
-            );
-          }else{
-            console.log(instruction.parsed)
-          }
-        }
-      });
-    }
-
-  } catch (err) {
-    console.log('err', signature, err)
-    await sleep(20000)
-    getTokenMint(signature);
-    return
+class TokenMintScanner {
+  constructor(signature) {
+    this.signature = signature;
+    this.RETRIES = 1;
+    this.MAX_RETRIES = 5;
+    this.connection = this.getConnection();
+    this.metaplex = Metaplex.make(this.connection);
   }
-}
 
+  sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
 
-
-async function getTokenSupply(mint) {
-  let supply = await web3.getTokenSupply(mint);
-  return supply.value.uiAmount;
-}
-
-async function getTokenMeta(mintAddress, signature) {
-  console.log(mintAddress)
-  let mint = new PublicKey(mintAddress);
-  let name;
-  let symbol;
-  let info;
-  let supply;
-  let logo;
-  let decimals;
-  let authority;
-
-  try {
-    const token = await metaplex
-      .nfts()
-      .findByMint({ mintAddress: mint });
-
-    name = token.json.name ? token.json.name : "";
-    symbol = token.json.symbol ? token.json.symbol : "";
-    info = token.json.description
-      ? token.json.description
-      : "";
-    logo = token.json.image ? token.json.image : false;
-    decimals = token.mint.decimals
-      ? token.mint.decimals
-      : "NOT SPECIFIED";
-    authority = token.updateAuthorityAddress.toString()
-      ? token.updateAuthorityAddress
-      : undefined;
-    let tokenSupply = await getTokenSupply(mint);
-    supply = formatNumber(tokenSupply);
-    if (token.mint.decimals === 0 ){
-      console.log("Token decimals is 0, maybe nft??")
-      return
+  getConnection() {
+    //Ist Connection Instance
+    if (this.RETRIES === 1) {
+      return new Connection(Config.CONNECTION1)
     }
-    
+    //Second Connection Instance
+    else if (this.RETRIES === 2) {
+      return new Connection(Config.CONNECTION2)
+    }
+    //Third Connection Instance
+    else if (this.RETRIES === 3) {
+      return new Connection(Config.CONNECTION3)
+    }
+    //Fourth Connection Instance
+    else if (this.RETRIES === 4) {
+      return new Connection(Config.CONNECTION4)
+    }
+    //Last Connection Instance
+    else {
+      return new Connection(Config.CONNECTION5)
+    }
+  }
 
-    let msg = `New  Mint » <a href="https://solscan.io/token/${mintAddress}">${name}</a>\n\n<code>${mint}</code>\n\n👉 ${supply} Minted\n\nOwner: <a href="https://solscan.io/account/${authority}">Deployer</a>\n\nChart: <a href="https://birdeye.so/token/${mint}">${name} (${symbol}) » Birdeye</a>\n\n${info}`;
-    const keyboards = [
-      [
-        Markup.button.url(
-          "Trade on soltrader",
-          `https://solscan.io/tx/${signature}`
-        )
-      ],
-    ];
-    if (logo) {
-      await bot.telegram.sendPhoto(
-        Config.TELEGRAM_CHAT_ID,
-        logo,
+  async getTokenMint() {
+    try {
+      if (this.RETRIES >= this.MAX_RETRIES) {
+        //we can't continue if rpc error
+        return
+      }
+      await this.sleep(15000)
+      const transaction = await this.connection.getParsedTransaction(
+        signature,
         {
-          caption: msg,
+          maxSupportedTransactionVersion: 0,
+        }
+      );
+      if (transaction && transaction.transaction) {
+        transaction.transaction.message.instructions.forEach(instruction => {
+          // logic to verify if this instruction is a token creation
+          if (instruction.parsed && instruction.parsed.type) {
+            if (instruction.parsed.type == "initializeMint") {
+              this.getTokenMeta(
+                instruction.parsed.info.mint, signature
+              );
+            } else if (instruction.parsed.type == "InitializeMint") {
+              this.getTokenMeta(
+                instruction.parsed.info.mint, signature
+              );
+            } else if (instruction.parsed.type == "initializeMint2") {
+              this.getTokenMeta(
+                instruction.parsed.info.mint, signature
+              );
+            }
+            else if (instruction.parsed.type == "InitializeMint2") {
+              this.getTokenMeta(
+                instruction.parsed.info.mint, signature
+              );
+            }
+          }
+        });
+      }
+
+    } catch (err) {
+      console.log("eror", this.RETRIES,this.signature)
+      //wait for 5secs
+      await this.sleep(5000)
+      // Increment tries
+      this.RETRIES++;
+      //retry mint 
+      this.getTokenMint()
+    }
+  }
+
+
+  
+  async getTokenSupply(mint) {
+    /**
+     * @getTokenSupply
+     * @param {PublicKey} mint: the token mint address
+     * @returns {int} total token mint
+     */
+    const supply = await this.connection.getTokenSupply(mint);
+    return supply.value.uiAmount;
+  }
+  
+
+  async  getTokenMeta(mintAddress) {
+    console.log(mintAddress)
+    let mint = new PublicKey(mintAddress);
+    let name;
+    let symbol;
+    let info;
+    let supply;
+    let logo;
+    let authority;
+  
+    try {
+      const token = await this.metaplex
+        .nfts()
+        .findByMint({ mintAddress: mint });
+  
+      name = token.json.name ? token.json.name : "";
+      symbol = token.json.symbol ? token.json.symbol : "";
+      info = token.json.description
+        ? token.json.description
+        : "";
+      logo = token.json.image ? token.json.image : false;
+      authority = token.updateAuthorityAddress.toString()
+        ? token.updateAuthorityAddress
+        : "";
+      let tokenSupply = await getTokenSupply(mint);
+      supply = formatNumber(tokenSupply);
+      if (token.mint.decimals === 0) {
+        //Token decimals is 0, maybe nft??
+        return
+      }
+  
+      let msg = `New  Mint » <a href="https://explorer.solana.com/token/${mintAddress}">${name}</a>\n\n<code>${mint}</code>\n\n👉 ${supply} Minted\n\nOwner: <a href="https://explorer.solana.com/account/${authority}">Deployer</a>\n\nChart: <a href="https://birdeye.so/token/${mint}">${name} (${symbol}) » Birdeye</a>\n\n${info}`;
+      const keyboards = [
+        [
+          Markup.button.url(
+            "",
+            `https://explorer.solana.com/tx/${signature}`
+          )
+        ],
+      ];
+      if (logo) {
+        await bot.telegram.sendPhoto(
+          Config.TELEGRAM_CHAT_ID,
+          logo,
+          {
+            caption: msg,
+            parse_mode: "HTML",
+            /*reply_markup: {
+              inline_keyboard: keyboards,
+            },*/
+          }
+        );
+        return;
+      }
+      await bot.telegram.sendMessage(
+        Config.TELEGRAM_CHAT_ID,
+        msg,
+        {
           parse_mode: "HTML",
           /*reply_markup: {
             inline_keyboard: keyboards,
@@ -149,23 +201,18 @@ async function getTokenMeta(mintAddress, signature) {
         }
       );
       return;
+    } catch (e) {
+      console.log("metaplex not fetching token", e);
+      //you can keep running if you wish
+      //await getTokenMeta(mintAddress);
     }
-    await bot.telegram.sendMessage(
-      Config.TELEGRAM_CHAT_ID,
-      msg,
-      {
-        parse_mode: "HTML",
-        /*reply_markup: {
-          inline_keyboard: keyboards,
-        },*/
-      }
-    );
-    return;
-  } catch (e) {
-    console.log("metaplex not fetching token", e);
-    await sleep(20000);
-    //await getTokenMeta(mintAddress);
   }
+
 }
+
+
+
+
+
 
 startEvent()
